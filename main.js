7ee1e228b1850653971eb702796d832d9d08f1c4
Oliver <draper87@gmail.com>
2020-06-08 16:50:17 +0200

    Fix

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
 // aggiungo la classe .active quando clicco sul hamburger
-$('a i.fas.fa-bars').click(
+$('.header-right > a').click(
   function() {
     $('.hamburger-menu').addClass('active');
   }
 );
 
 // tolgo la classe .active quando clicco sulla X
-$('a i.fas.fa-times').click(
+$('.hamburger-menu > a').click(
   function() {
     $('.hamburger-menu').removeClass('active');
   }
